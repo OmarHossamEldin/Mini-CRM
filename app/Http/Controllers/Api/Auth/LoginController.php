80d6cea59a46d4c6df6fa8a00ee637ec509d487c
OmarHossamEldin <okandil273@gmail.com>
2021-06-19 18:10:16 +0200

    finishing-collection-api

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\AuthRepository;
 use App\Http\Requests\Auth\LoginRequest;
-use Lang;
 
 class LoginController extends Controller
 {
@@ -13,27 +13,13 @@ class LoginController extends Controller
      * we need to replace later to convert,
      * it to jwt token for more security
      * @param  App\Http\Requests\Auth\LoginRequest  $request
+     * @param  App\Repositories\AuthenticateRepository $authRepository
      * @return Illuminate\Http\Response
      */
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request, AuthRepository $authRepository)
     {
-        $validatedData = $request->validated();
-
-        if (Auth::attempt($validatedData)) {
-            $user = Auth::user();
-            $user->ApiTokenGenerater();
-            $user = $user->only(['name', 'email', 'api_token']);
-            return response()->json([
-                'message' => Lang::get('auth.success'),
-                'errors' => [],
-                'user' => $user,
-            ], 200);
-        } else {
-            return response()->json([
-                'message' => [],
-                'errors' => [Lang::get('auth.failed')],
-                'user' => [],
-            ], 200);
-        }
+        $response = $authRepository->authenticate($request->validated());
+        
+        return $response['message'] ? response()->json($response, 201) : response()->json($response, 401);
     }
 }
--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -4,7 +4,26 @@ namespace App\Repositories;
 
 use Illuminate\Support\Facades\Auth;
 
-class CompanyRepository  
+class AuthRepository
 {
-   
+    public function authenticate(array $data): array
+    {
+        if (Auth::attempt($data)) {
+            $user = Auth::user();
+            $user->ApiTokenGenerater();
+            $user = $user->only(['name', 'email', 'api_token']);
+            $response = [
+                'message' => 'you have been loged in successfully',
+                'errors' => [],
+                'user' => $user,
+            ];
+        } else {
+            $response = [
+                'message' => [],
+                'errors' => 'your login credentials not matches our records',
+                'user' => []
+            ];
+        }
+        return $response;
+    }
 }
